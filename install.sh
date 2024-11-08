dcbbacdfde2bda1afb5bd5b993acae0811cb4320
Ehsan <e.zandi@gmail.com>
2024-11-08 12:53:41 +0100

    for showing line numbers

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 vim_dir=".jupyter/lab/user-settings/@axlair/jupyterlab_vim"
 theme_dir=".jupyter/lab/user-settings/@jupyterlab/apputils-extension"
+nb_extension_dir=".jupyter/lab/user-settings/@jupyterlab/notebook-extension"
+
 
 if [ ! -d  "$HOME/$vim_dir" ]; then 
   mkdir -p "$HOME/$vim_dir"
@@ -14,4 +16,9 @@ if [ ! -d  "$HOME/$theme_dir" ]; then
   mkdir -p "$HOME/$theme_dir"
 fi
 ln -sf "$PWD/$theme_dir/themes.jupyterlab-settings" "$HOME/$theme_dir"
+
+if [ ! -d  "$HOME/$nb_extension_dir" ]; then 
+  mkdir -p "$HOME/$nb_extension_dir"
+fi
+ln -sf "$PWD/$nb_extension_dir/tracker.jupyterlab-settings" "$HOME/$nb_extension_dir"
 
